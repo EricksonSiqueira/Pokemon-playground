e43f43f6fe7c26d3273dfca4c22d99baffa00019
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-01-18 00:03:26 -0300

    test: create first Home test

---
+++ src/tests/Home.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../pages';
+
+describe('Home', () => {
+  it('renders a h1 with text "Welcome to the best Pokedex!"', () => {
+    render(<Home />);
+
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+        name: 'Welcome to the best Pokedex!',
+      })
+    ).toBeInTheDocument();
+  });
+});
