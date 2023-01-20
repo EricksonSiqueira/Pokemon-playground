9f4a92db18e1197aaad788e6e82be9b6f9ed8577
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-01-19 21:50:56 -0300

    feat: create SEO component to handle page Head

---
+++ src/components/SEO/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+
+import React from 'react';
+
+export interface SEOProps {
+  title: string;
+}
+
+function SEO({ title }: SEOProps) {
+  return (
+    <Head>
+      <title>{title && `Pokedex - by Erickson`}</title>
+      <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="MobileOptimized" content="320" />
+      <meta name="HandheldFriendly" content="True" />
+      <meta name="google" content="notranslate" />
+      <meta property="og:locale" content="pt_BR" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={title} />
+      <meta property="og:image:alt" content="Thumbnail" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:image:alt" content="Thumbnail" />
+      <meta name="twitter:image:width" content="1200" />
+      <meta name="twitter:image:height" content="620" />
+    </Head>
+  );
+}
+
+export default React.memo(SEO);
