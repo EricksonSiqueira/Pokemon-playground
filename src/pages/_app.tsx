9de1989686a7375367ef01135326d6c88b50146b
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-01-30 20:17:57 -0300

    feat: add font rubik as default for the web site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Rubik } from '@next/font/google';
+
+// eslint-disable-next-line prettier/prettier, @typescript-eslint/quotes
+const rubik = Rubik({
+  // eslint-disable-next-line prettier/prettier, @typescript-eslint/quotes
+  subsets: ['latin'],
+  // eslint-disable-next-line @typescript-eslint/quotes
+  weight: '400',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+        html {
+          font-family: ${rubik.style.fontFamily};
+        }
+      `}</style>
+    </>
+  );
 }
