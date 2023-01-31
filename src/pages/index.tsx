d3e7baa93539c91af6ce9b6894521c3b4b8e2164
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2023-01-31 19:27:39 -0300

    feat: pass Home main content inside of Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout />
-      <main>
-        <h1 className="text-red-900 text-9xl">Welcome to the best Pokedex!</h1>
-      </main>
+      <Layout>
+        <main>
+          <h1 className="text-red-900 text-9xl">
+            Welcome to the best Pokedex!
+          </h1>
+        </main>
+      </Layout>
     </>
   );
 }
